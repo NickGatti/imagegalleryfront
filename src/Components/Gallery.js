96c567af355444af7064b97a9926ee17a04c1932
Nick Gatti <nick.gatti@gmail.com>
2022-12-29 19:04:06 -0700

    added gallery

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,11 +1,17 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 
 export const Gallery = () => {
+    const [galleryImages, setGalleryImages] = useState()
+
     useEffect(() => {
         fetch('/images').then(res => {
             return res.json()
         }).then(res => {
             console.log('GET /images Success!', res)
+            setGalleryImages(res.images.map((image, idx) => {
+                return <img key={idx} src={image.url} />
+            }))
+
         }).catch(err => {
             console.log('ERROR on GET /images ERROR:', err)
         })
@@ -13,7 +19,7 @@ export const Gallery = () => {
 
     return (
         <div>
-            SAMPLE IMAGE
+            {galleryImages}
         </div>
     )
 }
